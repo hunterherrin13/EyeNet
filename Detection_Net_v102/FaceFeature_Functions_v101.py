884c251bf73cefd24a0e3019628705e979a97ba7
hunterherrin13 <100969266+hunterherrin13@users.noreply.github.com>
2024-03-01 20:11:18 -0800

    FaceFeature - model adjustment

--- Detection_Net_v102/FaceFeature_Functions_v101.py
+++ Detection_Net_v102/FaceFeature_Functions_v101.py
@@ -1,4 +1,38 @@
-from facenet_pytorch import InceptionResnetV1
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import numpy as np
+import pandas as pd
+import os
 
-# For a model pretrained on VGGFace2
-model = InceptionResnetV1(pretrained='vggface2').eval()
+workers = 0 if os.name == 'nt' else 4
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+
+mtcnn = MTCNN(
+    image_size=160, margin=0, min_face_size=20,
+    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+    device=device
+)
+# help(MTCNN)
+
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+def collate_fn(x):
+    return x[0]
+
+dataset = datasets.ImageFolder('../data/test_images')
+dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
+loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
+
+aligned = []
+names = []
+for x, y in loader:
+    x_aligned, prob = mtcnn(x, return_prob=True)
+    if x_aligned is not None:
+        print('Face detected with probability: {:8f}'.format(prob))
+        aligned.append(x_aligned)
+        names.append(dataset.idx_to_class[y])
